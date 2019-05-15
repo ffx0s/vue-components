77b54cee852345c1d45405f6de53d737d0fe8a23
ffx0s <a13589949@gmail.com>
2019-05-15 14:18:21 +0800

    添加圆形进度条组件

---
+++ src/views/ring/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Navbar title="Ring" />
+    <div class="page-spacing">
+      <Ring v-model="progress" />
+      <br />
+      <Ring
+        v-model="progress"
+        :size="80"
+        :strokeWidth="0"
+        :innerStrokeWidth="5"
+        color="#fff"
+        textColor="#fff"
+        backgroundColor="rgba(0,0,0,.5)"
+        outerColor="rgba(0,0,0,.5)"
+      />
+      <br />
+      <VButton type="primary" @click="add">增加10%</VButton>
+      <VButton @click="sub">减少10%</VButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import Ring from '../../components/ring'
+
+export default {
+  components: {
+    Ring
+  },
+  data() {
+    return {
+      progress: 10
+    }
+  },
+  methods: {
+    sub() {
+      if (this.progress === 0) return
+      this.progress -= 10
+    },
+    add() {
+      if (this.progress === 100) return
+      this.progress += 10
+    }
+  }
+}
+</script>
