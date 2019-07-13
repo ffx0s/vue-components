11814f66d224f58f4ab0e1160ee1edd20ea40855
ffx0s <a13589949@gmail.com>
2019-07-13 17:50:59 +0800

    配置更新

--- example/vue.config.js
+++ example/vue.config.js
@@ -1,16 +1,30 @@
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin')
 const path = require('path')
+
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  configureWebpack: {
+    plugins: [new InlineManifestWebpackPlugin('manifest')]
+  },
   chainWebpack: config => {
-    config.devtool('eval-source-map')
     config.resolve.alias.set(
       'vue$',
-      resolve('./node_modules/vue/dist/vue.esm.js')
+      resolve('./node_modules/vue/dist/vue.runtime.esm.js')
     )
+    config.optimization.runtimeChunk({ name: 'manifest' })
+    config.plugin('html').tap(args => {
+      args[0].inject = false
+      return args
+    })
+    if (process.env.NODE_ENV === 'production') {
+      //
+    } else {
+      config.devtool('eval-source-map')
+    }
   },
   css: {
     extract: false
